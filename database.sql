7e67ab8c1e844365ad95b9ca2dd58e788176229c
Moamiine <medamine.teljaoui@gmail.com>
2025-12-15 14:25:04 +0100

    created tables in database

--- database.sql
+++ database.sql
@@ -1,2 +1,15 @@
+CREATE TABLE User (
+    id INT PRIMARY KEY,
+    nome VARCHAR(100),
+    mdp VARCHAR(100),
+    date_inscription DATE
+);
+CREATE TABLE contact (
+    id INT PRIMARY KEY,
+    nom VARCHAR(100),
+    telephone INT,
+    email VARCHAR(100),
+    adresse VARCHAR(255)
+);
 
 
